631594bfff4ec075a8452710e2b5dcf43b4f197c
Olvet7 <volya.maslova@gmail.com>
2024-03-15 15:26:16 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
 
 export default function App() {
   return (
     <>
-    <h1>Hi React Router Dom</h1>
-    <Routes>
-      <Route path="/" element={<div>Home page</div>}/>
-    </Routes>
+      <h1>Hi React Router Dom</h1>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/payments" element={<div>Payments page</div>} />
+        <Route path="*" element={<div>Opps...Page is not found</div>}/>
+      </Routes>
     </>
   );
 }
